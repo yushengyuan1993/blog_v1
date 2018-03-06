9d45f9de2de9694843858b63e23d2a157a116591
yushengyuan <yushengyuan@xmebank.com>
2018-03-06 14:31:28 +0800

    add baidu hm

--- themes/yvshare/source/js/livere.js
+++ themes/yvshare/source/js/livere.js
@@ -18,3 +18,14 @@ e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
 })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
 _st('install','MzwgcVuvG9yJ7Q2Pu2Nd','2.0.0');
+
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eb7d549595bfdb15f99f36861d29c30d";
+//   var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("head")[0]; 
+  s.appendChild(hm);
+//   s.parentNode.insertBefore(hm, s);
+})();
